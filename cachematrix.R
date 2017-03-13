0c1229e70f431365a2ca8034b2a22316a3a20462
Trish Johnston <trishaphish@gmail.com>
2017-03-13 13:53:39 -0700

    Update cachematrix.R
    Copied from trishaphish-PA2 branch

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix helps create a matrix that can store a copy of it's inverse
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+## this creates a matrix that functions as a list.
+## inv_mat represents the matrix, set_inv is a function to set the inverse
+## get_inv calls the saved matrix and will be used in cacheSholve below
+        
+        inv_mat <- NULL
+        set <- function(y) {
+                x <<- y
+                inv_mat <<- NULL
+        }
+        get <- function() x
+        set_inv <- function(inverse) inv_mat <<- inverse
+        get_inv <- function() inv_mat
+        
+        list(
+                set = set,
+                get = get,
+                set_inv = set_inv,
+                get_inv = get_inv
+        )
 }
 
 
-## Write a short comment describing this function
+## cacheSolve creates an inverse of a matrix.  But first, it checks against
+## makeCacheMatrix for a saved inverse matrix file.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv_mat <- x$get_inv()
+        if(!is.null(inv_mat)) {
+                message("getting cached data")
+                return(inv_mat)
+        }
+        data <- x$get()
+        inv_mat <- solve(data, ...)
+        x$set_inv(inv_mat)
+        inv_mat
 }
